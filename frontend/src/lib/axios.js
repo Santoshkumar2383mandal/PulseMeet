5cb7f72180b38afeec81312580d7c1e9257258c4
Santosh Kumar Mandal <kennikurenchi2383@gmail.com>
2025-08-25 02:02:27 +0530

    fix: update axios base URL and add Vercel configuration for rewrites

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL || "https://pulsemeet-1.onrender.com/";
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:5001/api",
+    // baseURL : "http://localhost:5001/api",
+    baseURL :"BASE_URL",
     withCredentials:true //send cookie with request
 });
