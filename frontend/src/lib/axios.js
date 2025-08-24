f31ce4bf9d8cbe23b0e252274654a49d64a75cf9
Santosh Kumar Mandal <kennikurenchi2383@gmail.com>
2025-08-25 02:12:18 +0530

    fix: update axios base URL to include API path

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "https://pulsemeet-1.onrender.com/";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://pulsemeet-1.onrender.com/api";
 export const axiosInstance = axios.create({
     // baseURL : "http://localhost:5001/api",
     baseURL :"BASE_URL",
